b04312bd2dcf3cfbb5768d4ca66c3a6c1eaabf49
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-07-28 02:17:06 +0300

    now after creating new category it will redirect to the categories list unless if the user clicked Create & create another ✨ - also for the user creation

--- app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -9,4 +9,12 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+
+
+    // this function should redirect to the index page after creating a record
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -9,4 +9,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    // this function should redirect to the index page after creating a record
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
