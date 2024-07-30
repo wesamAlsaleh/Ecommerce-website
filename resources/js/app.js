cd282afa0aafcc3b693b67151b7fb8705b2f3d45
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-07-31 00:19:24 +0300

    feat: Add event listener for Livewire navigation events - this will fix the bug that make the hamburger navbar button in the mobile media not working without refresh 🟢

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,8 @@
 import "./bootstrap";
 
 import "preline";
+
+document.addEventListener("livewire:navigated", () => {
+    // Triggered as the final step of any page navigation...
+    // Also triggered on page-load instead of "DOMContentLoaded"...
+});
