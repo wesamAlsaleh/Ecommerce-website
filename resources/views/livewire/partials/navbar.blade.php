5d640ad6241f1ce22898d86fff6ae0e09edc2580
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-07-30 18:09:28 +0300

    added Livewire's wire:navigate feature to  make the pages navigation much faster 🔥

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -38,41 +38,54 @@
                         class="flex flex-col gap-x-0 mt-5 divide-y divide-dashed divide-gray-200 md:flex-row md:items-center md:justify-end md:gap-x-7 md:mt-0 md:ps-7 md:divide-y-0 md:divide-solid dark:divide-gray-700">
 
                         <a class="font-bold {{ request()->is('/') ? 'text-blue-600' : 'text-gray-500' }} hover:text-gray-400 py-3 md:py-6 dark:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 "
-                            href="/" aria-current="page">
+                            href="/" aria-current="page" wire:navigate>
                             Home
                         </a>
 
                         <a class="font-semibold {{ request()->is('categories') ? 'text-blue-600' : 'text-gray-500' }}  hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 "
-                            href="/categories">
+                            href="/categories" wire:navigate>
                             Categories
                         </a>
 
                         <a class="font-semibold {{ request()->is('products') ? 'text-blue-600' : 'text-gray-500' }}  hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 "
-                            href="/products">
+                            href="/products" wire:navigate>
                             Products
                         </a>
 
                         <a class="font-bold {{ request()->is('cart') ? 'text-blue-600' : 'text-gray-500' }} flex items-center  hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                            href="/cart">
+                            href="/cart" wire:navigate>
+                            {{-- cart icon --}}
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="1.5" stroke="currentColor" class="flex-shrink-0 w-5 h-5 mr-1">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                             </svg>
-                            <span class="mr-1">Cart</span> <span
-                                class="py-0.5 px-1.5 rounded-full text-xs font-medium bg-blue-50 border border-blue-200 text-blue-600">4</span>
+
+                            {{-- text --}}
+                            <span class="mr-1">Cart</span>
+
+                            {{-- number of order items --}}
+                            <span
+                                class="py-0.5 px-1.5 rounded-full text-xs font-medium bg-blue-50 border border-blue-200 text-blue-600">
+                                4
+                            </span>
                         </a>
 
                         <div class="pt-3 md:pt-0">
                             <a class="py-2.5 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                                href="/login">
+                                href="/login" wire:navigate>
+
+                                {{-- login icon --}}
                                 <svg class="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24"
                                     height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                     stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                                     <circle cx="12" cy="7" r="4" />
                                 </svg>
-                                Log in
+
+                                <span>
+                                    Log in
+                                </span>
                             </a>
                         </div>
 
