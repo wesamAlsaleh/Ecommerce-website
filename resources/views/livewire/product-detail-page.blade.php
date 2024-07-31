291205b45759410949a6fff7e005d1940792cc3b
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-07-31 16:23:10 +0300

    this is all i can do in the moment for the text rich - the dot is important for the new line

--- resources/views/livewire/product-detail-page.blade.php
+++ resources/views/livewire/product-detail-page.blade.php
@@ -5,7 +5,7 @@
 
                 {{-- images container --}}
                 <div class="w-full mb-8 md:w-1/2 md:mb-0" x-data="{ mainImage: '{{ url('storage/' . $product->images[0]) }}' }">
-                    <div class="sticky top-0 z-50 overflow-hidden ">
+                    <div class="top-0 z-50 overflow-hidden">
                         {{-- the main image --}}
                         <div class="relative mb-6 lg:mb-10 lg:h-2/4 ">
                             <img x-bind:src="mainImage" alt="{{ $product->name }}"
