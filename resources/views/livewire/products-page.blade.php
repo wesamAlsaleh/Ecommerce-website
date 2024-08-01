75acd436f120770e0f8b31939c2c3ae72674f997
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-08-01 22:20:23 +0300

    chore: Update add to cart button UI and functionality on product detail page

--- resources/views/livewire/products-page.blade.php
+++ resources/views/livewire/products-page.blade.php
@@ -154,26 +154,34 @@
 
                                     {{-- add to cart button --}}
                                     <div class="flex justify-center p-4 border-t border-gray-300 dark:border-gray-700">
-                                        {{-- wire:click.prevent to add the product to the cart without reloading the page --}}
-                                        <a href="#" wire:click.prevent='addToCart({{ $product->id }})'
-                                            class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                fill="currentColor" class="w-4 h-4 bi bi-cart3 " viewBox="0 0 16 16">
-                                                <path
-                                                    d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z">
-                                                </path>
-                                            </svg>
+                                        @if ($product->in_stock > 0)
+                                            {{-- wire:click.prevent to add the product to the cart without reloading the page --}}
+                                            <a href="#" wire:click.prevent='addToCart({{ $product->id }})'
+                                                class="text-gray-500 flex items-center space-x-2 dark:text-gray-400 hover:text-red-500 dark:hover:text-red-300">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                    fill="currentColor" class="w-4 h-4 bi bi-cart3 "
+                                                    viewBox="0 0 16 16">
+                                                    <path
+                                                        d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z">
+                                                    </path>
+                                                </svg>
 
-                                            {{-- add to cart text --}}
-                                            <span wire:loading.remove wire:target="addToCart({{ $product->id }})">
-                                                Add to Cart
-                                            </span>
+                                                {{-- add to cart text --}}
+                                                <span wire:loading.remove
+                                                    wire:target="addToCart({{ $product->id }})">
+                                                    Add to Cart
+                                                </span>
 
-                                            {{-- animation for the adding to cart using livewire stuff --}}
-                                            <span wire:loading wire:target="addToCart({{ $product->id }})">
-                                                Adding to Cart...
+                                                {{-- animation for the adding to cart using livewire stuff --}}
+                                                <span wire:loading wire:target="addToCart({{ $product->id }})">
+                                                    Adding to Cart...
+                                                </span>
+                                            </a>
+                                        @else
+                                            <span class="text-red-600 dark:text-red-400 font-semibold">
+                                                Out of Stock
                                             </span>
-                                        </a>
+                                        @endif
                                     </div>
                                 </div>
                             </div>
