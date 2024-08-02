47e433b3ac033fd8fe206e9a6be70fb57a2cdf5d
Wesam <97985709+wesamAlsaleh@users.noreply.github.com>
2024-08-02 17:24:12 +0300

    error handling - 20

--- app/Livewire/Auth/ForgotPage.php
+++ app/Livewire/Auth/ForgotPage.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Auth;
 
 use Illuminate\Support\Facades\Password;
 use Livewire\Component;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ForgotPage extends Component
 {
@@ -16,11 +17,17 @@ class ForgotPage extends Component
             'email' => 'required|email',
         ]);
 
-        $status = Password::sendResetLink(['email' => $this->email]);
+        try {
+            // dd($this->email);
+            $credentials = ['email' => $this->email];
 
-        if ($status === Password::RESET_LINK_SENT) {
-            session()->flash('success', 'Reset password link sent successfully.');
-        } else {
+            $status = Password::sendResetLink($credentials);
+
+            if ($status === Password::RESET_LINK_SENT) {
+                session()->flash('success', 'Reset password link sent successfully.');
+                $this->email = '';
+            }
+        } catch (\Exception $e) {
             session()->flash('error', 'Unable to send reset password link.');
         }
     }
